0dd7519d0f932b858036c887ecba3fb8087049f6
sanchan <sanchan@coderknock.com>
2022-04-08 23:25:29 +0800

    初始化项目

---
+++ croe/src/main/java/com/coderknock/apigateway/core/Bootstrap.java
@@ -0,0 +1,14 @@
+package com.coderknock.apigateway.core;
+
+/**
+ * 启动类
+ *
+ * @author sanchan
+ * @微信 sanchan_coderknock
+ * @B站 https://space.bilibili.com/62450448
+ * @CSDN https://blog.csdn.net/sanchan
+ * @WebSite https://coderknock.com
+ * @date 2022-04-08
+ */
+public class Bootstrap {
+}
